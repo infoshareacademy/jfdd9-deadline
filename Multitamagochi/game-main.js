d39c5d8065d7557e76666e98e00ba1ee414deefd
Gerard Szulc <gerszulc05@gmail.com>
2018-04-11 15:55:28 +0200

    initial game commit

---
+++ Multitamagochi/game-main.js
@@ -0,0 +1,12 @@
+var game = (function(){
+
+    var gameBox = document.getElementById('game');
+    var pointsNode = document.getElementById('points');
+    var catsNode = document.getElementById('cats');
+    var itemsNode = document.getElementById('items');
+
+
+
+
+
+})();
