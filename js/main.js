a0fb4ff66a0a9fe5aa106cb198d392a2c3a120c8
EwaK <klebba.ewa@gmail.com>
2018-04-12 16:56:34 +0200

    dodano podświetlenie linku w nawbarze aktywnej sekcji

--- js/main.js
+++ js/main.js
@@ -7,4 +7,34 @@ $('.nav-link').click(function(event){
 });
 
 
-//todo window.location.href
+var links = document.querySelectorAll('.nav-link');
+var menu = document.querySelector('nav');
+var hero = document.querySelector('header.hero');
+
+window.addEventListener('scroll', function (event) {
+    var currentPosition = window.pageYOffset;
+    Array.prototype.map.call(links, function (link) {
+        return document.querySelector(
+            link.getAttribute('href')
+        )
+    }).forEach(function (element, index) {
+        var pos = currentPosition + menu.offsetHeight + 1;
+        if (
+            pos > element.offsetTop &&
+            pos < element.offsetHeight + element.offsetTop) {
+            links.forEach(function (link) {
+                link.parentNode.classList.remove('active')
+            });
+            links[index].parentNode.classList.add('active')
+        }
+
+
+        console.log(hero.offsetHeight);
+        if (currentPosition < hero.offsetHeight){
+            links.forEach(function (link) {
+                link.parentNode.classList.remove('active')
+            });
+        }
+    })
+});
+
