1fa916d9b7bc1c41cbfa72cbee932bd90b583d16
Gerard Szulc <gerszulc05@gmail.com>
2018-04-12 17:01:56 +0200

    klikanie itemów

--- Multitamagochi/game-main.js
+++ Multitamagochi/game-main.js
@@ -6,12 +6,18 @@
     var pointsNode = document.getElementById('points');
     var catsNode = document.getElementById('cats');
     var itemsNode = document.getElementById('items');
+    var foodNode = document.getElementById('food');
+    var handNode = document.getElementById('hand');
+    var brushNode = document.getElementById('brush');
+    var woolNode = document.getElementById('wool');
     var height = 2;
     var width = 2;
     var randomNeedIndex;
     var timeNode = document.getElementById('timer');
     //var needsTable = needsNode.appendChild(createNeedsTable(width,height));
   //  var needsTd = needsTable.querySelectorAll('td');
+
+    /*licznik czasu */
     var timeV = 0;
 
 
@@ -19,8 +25,7 @@
         setInterval(function(){
             timeV+=1;
             timeNode.innerHTML = timeV + 'seconds';
-            console.log(timeV);
-            if(timeV === 5){
+            if(timeV === 10){
                 timeV = 0;
             }
 
@@ -28,140 +33,103 @@
             return timeV;
 
     })();
-
+/* losowanie liczb z zakresu range*/
     function getNumberFromRange(range) {
         return Math.floor(Math.random() * range);
     }
-
+/* losowanie potrzeby*/
 function randomNeed(){
         randomNeedIndex = getNumberFromRange(4);
-        needsNode.innerText = 'Need index' + randomNeedIndex;
-    console.log(randomNeedIndex);
-    if(randomNeedIndex === 0){
-        needsNode.classList.add('food')
+        needsNode.innerText = 'Need index ' + randomNeedIndex;
+    var needs = ['food', 'hand', 'brush', 'wool'];
+    needsNode.classList.add(needs[randomNeedIndex]);
 
-    }
-    if(randomNeedIndex === 1){
-        needsNode.classList.add('hand')
-
-    }
-    if(randomNeedIndex === 2){
-        needsNode.classList.add('brush')
-
-    }
-    if(randomNeedIndex === 3){
-        needsNode.classList.add('wool')
-    }
 
     setTimeout(function(){
         needsNode.className = '';
-
-
-
+        needsNode.innerHTML = '';
     },3000);
     return randomNeedIndex;
 }
 
 function needRandomizer(tim) {setInterval(randomNeed,tim)
 }
+needRandomizer((getNumberFromRange(4000)));
+    var item = itemsNode.querySelectorAll('div');
 
-needRandomizer(5000);
-needRandomizer(1500);
-needRandomizer(1000);
-needRandomizer(800);
-
-
-
-
-
-/*
-    function createNeedsTable(width, height) {
-        var table = document.createElement('table');
-        var tbody = document.createElement('tbody');
-        var tr, td, x, y;
-
-        for (y = 0; y < height; y += 1) {
-            tr = document.createElement('tr');
-            for (x = 0; x < width; x += 1) {
-                td = document.createElement('td');
-                tr.appendChild(td)
-                td.innerText = '0';
+var activeItem = false;
+ // dodawanie eventlistenera
+    itemsNode.addEventListener('click',function (event) {
+        var clickedElement = event.target;
+        if (activeItem !== false){
+            if( activeItem !== clickedElement){
+                return
             }
-            tbody.appendChild(tr)
         }
-
-        table.appendChild(tbody);
-        return table;
-    }
-
-
-
-    function needTimer() {
-
-        setInterval(function (){
-            randomNeed = getNumberFromRange(needsTd.length);
-            console.log(randomNeed)
-        }, 400);
-    }
-
-    function showFood(){
-        var foodImage = document.createElement("img");
-         var addFoodImage = document.getElementsByTagName('td').appendChild(foodImage);
-         addFoodImage.classList.add('.food')
-
-    }
-    function showHand(){
-        var handImage = document.createElement("img");
-        var addHandImage =  document.getElementsByTagName('td').appendChild(handImage);
-        addHandImage.classList.add('.hand')
-
-    }
-    function showBrush(){
-        var brushImage = document.createElement("img");
-         var addBrushImage = document.getElementsByTagName('td').appendChild(brushImage);
-        addBrushImage.classList.add('.brush')
-
-    }
-    function showWool(){
-        var woolImage = document.createElement("img");
-         var addWoolImage = document.getElementsByTagName('td').appendChild(woolImage);
-        addWoolImage.classList.add('.wool')
-
-    }
-
-    function needRandomizer(){
-        setInterval(function(){
-        needTimer();
+        if (clickedElement.classList.contains('item')){
+            if (!clickedElement.classList.contains('active_item')) {
+                clickedElement.classList.add('active_item');
+                activeItem = clickedElement;
 
 
-        if(randomNeed === 0){
-            showFood();
-            needsTd[randomNeed].innerHTML = '1';
-        }
-        if(randomNeed === 1){
-            showHand();
-            needsTd[randomNeed].innerText = '2';
+            } else {
+                clickedElement.classList.remove('active_item')
+                activeItem = false;
+
+            }
+            console.log(clickedElement);
+
 
         }
-        if(randomNeed === 2){
-            showBrush();
-            needsTd[randomNeed].innerText = '3';
+    });
+
+// for(var i=0;i < item.length; i++){
+//      item[i].addEventListener('click',function(){
+//       item[element].classList.add('active_item');
+//          console.log('lol')
+//
+//  })}
+ /*
+//food
+    item[0].addEventListener('click',function(){
+        if (item[0].classList.contains('active_item')) {
+            item[0].classList.remove('active_item');
+            console.log('lol')
+        } else {
+            item[3].classList.add('active_item');
+            console.log(' trol')
         }
-        if(randomNeed === 3){
-            showWool();
-            needsTd[randomNeed].innerText = '4';
+
+        //hand
+    item[1].addEventListener('click',function(){
+        if (item[1].classList.contains('active_item')) {
+            item[1].classList.remove('active_item');
+            console.log('lol')
+        } else {
+            item[1].classList.add('active_item');
+            console.log(' trol')
         }
-        setTimeout(function(){
-            if (!(needsTd[randomNeed].textContent === '0')) {
-    needsTd[randomNeed].innerText = '0';
-}
-        },3000)},5000)
 
+        //brush
+    item[2].addEventListener('click',function(){
+        if (item[2].classList.contains('active_item')) {
+            item[2].classList.remove('active_item');
+            console.log('lol')
+        } else {
+            item[2].classList.add('active_item');
+            console.log(' trol')
+        }
 
-    }
+        //wool
+    item[3].addEventListener('click',function(){
+        if (item[3].classList.contains('active_item')) {
+            item[3].classList.remove('active_item');
+            console.log('lol')
+        } else {
+            item[3].classList.add('active_item');
+            console.log(' trol')
+        }})
 
-    needRandomizer();
-
-*/
+        */
 
 
